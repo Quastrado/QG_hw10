75d3a03e2ddf8bf6d6d0e52b9ead3998ff193f9c
Quastrado <Ataxyswriter@yandex.ru>
2026-05-17 13:06:47 +0300

    done

--- src/test/java/homework10/GitHubIssuesTests.java
+++ src/test/java/homework10/GitHubIssuesTests.java
@@ -1,26 +1,75 @@
 package homework10;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Owner;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import steps.WebSteps;
 import testData.TestData;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.openqa.selenium.By.linkText;
 
 public class GitHubIssuesTests {
     TestData testData = new TestData();
 
     @Test
+    @DisplayName("Тест Selenide")
+    @Owner("Quastrado")
+    @Tag("test")
     public void SelenideIssuesTest() {
-        open("https://github.com");
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open(testData.startUrl);
         $(".header-search-button").click();
         $("#query-builder-test").setValue(testData.searchQuery);
         $("#query-builder-test").submit();
         $(linkText(testData.searchQuery)).click();
         $("#issues-tab").click();
         $(withText(testData.issueName)).should(Condition.exist);
+    }
+
+    @Test
+    @DisplayName("Тест со Steps")
+    @Owner("Quastrado")
+    @Tag("test")
+    public void StepsIssuesTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+        steps.openMainPage();
+        steps.clickSearchButton();
+        steps.searchRepository();
+        steps.clickIssues();
+        steps.existIssues();
+    }
+
+    @Test
+    @DisplayName("Тест с Lambda")
+    @Owner("Quastrado")
+    @Tag("test")
+    public void LambdaIssuesTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открытие главной страницы", () -> {
+            open(testData.startUrl);
+        });
+        step("Вызов поисковой строки", () -> {
+            $(".header-search-button").click();
+        });
+        step("Отправка поискового запроса", () -> {
+            $("#query-builder-test").setValue(testData.searchQuery);
+            $("#query-builder-test").submit();
+        });
+        step("Переход во вкладку Issues", () ->{
+            $(linkText(testData.searchQuery)).click();
+            $("#issues-tab").click();
+        });
+        step("Проверка наличия Issues", () ->{
+            $(withText(testData.issueName)).should(Condition.exist);
+        });
     }
 }
--- src/test/java/testData/TestData.java
+++ src/test/java/testData/TestData.java
@@ -1,6 +1,7 @@
 package testData;
 
 public class TestData {
+    public String startUrl = "https://github.com";
     public String searchQuery = "Quastrado/QG_hw10";
     public String issueName = "TestIssue";
 }
